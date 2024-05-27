ccf8ee947f420adba1a931621d53f0231f87c5b9
HerbTeaCup <132678572+HerbTeaCup@users.noreply.github.com>
2024-05-27 16:01:47 +0900

    게임, 인풋, 카메라 매니저 생성

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager _instance = null;
+
+    InputManager _input = null;
+
+    CameraManager _cam = new CameraManager();
+
+    //Property
+    public static GameManager Instance { get { Init(); return _instance; } }
+    public static InputManager Input { get { return Instance._input; } }
+    public static CameraManager Cam { get { return Instance._cam; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input.Updater();
+    }
+    private void LateUpdate()
+    {
+        Input.LateUpdater();
+    }
+
+    static void Init()
+    {
+        if (_instance == null)
+        {
+            GameObject temp = GameObject.Find("@GameManager");
+
+            if (temp == null)
+            {
+                temp = new GameObject("@GameManager");
+            }
+
+            temp.TryGetComponent<GameManager>(out _instance);
+            if (_instance == null) { _instance = temp.AddComponent<GameManager>(); }
+
+            temp.TryGetComponent<InputManager>(out _instance._input);
+            if (_instance._input == null) { _instance._input = temp.AddComponent<InputManager>(); }
+        }
+    }
+}
